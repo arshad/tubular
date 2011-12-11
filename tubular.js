62206c885dfb6fa46e8582624a473aea7a547d52
Arshad Chummun <arshad@chummun.com>
2011-12-12 03:41:15 +0400

    Removed js alerts

--- tubular.js
+++ tubular.js
@@ -2,8 +2,6 @@
   Drupal.behaviors.tubular = {
     attach: function(context) {
       $('body').tubular(Drupal.settings.tubular.videoID, Drupal.settings.tubular.wrapperID);
-      alert(Drupal.settings.tubular.wrapperID);
-      alert(Drupal.settings.tubular.videoID);
     }
   }
 })(jQuery);
