0481bc645baad2a7fe7ebe955b548de86e1bf67f
Arshad Chummun <arshad@chummun.com>
2011-12-12 14:36:05 +0400

    control buttons for video

--- tubular.js
+++ tubular.js
@@ -2,6 +2,38 @@
   Drupal.behaviors.tubular = {
     attach: function(context) {
       $('body').tubular(Drupal.settings.tubular.videoID, Drupal.settings.tubular.wrapperID);
+      
+      // Play/Pause
+      $('#videoPause').click(function() {
+        if ($(this).hasClass('videoPaused')) {
+          ytplayer.playVideo();
+          $(this).removeClass('videoPaused');
+        } else {
+          ytplayer.pauseVideo();
+          $(this).addClass('videoPaused');
+        }
+        return false;
+      });
+      
+      // Mute/Unmute
+      $('#videoMute').click(function() {
+        if ($(this).hasClass('videoMute')) {
+          ytplayer.mute();
+          $(this).removeClass('videoMute');
+        } else {
+          ytplayer.unMute();
+          $(this).addClass('videoMute');
+        }
+        return false;
+      });
+      
+      // Nuke video (for slow connections -- stops loading entirely)
+      $('#videoStop').click(function() {
+        ytplayer.stopVideo();
+        ytplayer.clearVideo();
+        $('#yt-container').hide();
+        return false;
+      });
     }
   }
 })(jQuery);
